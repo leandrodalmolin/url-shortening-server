cc5c21db68b0b7e3d320033cf534a06bbc907df2
Leandro Piovezan Dal Molin <leandro.swk@hotmail.com>
2022-08-24 23:40:42 +0100

    Security - adds sanitization and validation to API inputs
    Installed "express-validator" to validate and sanitize inputs and installed "express-mongo-sanitize" to prevent MongoDB injection

--- src/functions/url.js
+++ src/functions/url.js
@@ -3,11 +3,21 @@ const serverless = require("serverless-http");
 const express = require('express');
 const cors = require('cors');
 const routes = require('../routes');
+const mongoSanitize = require('express-mongo-sanitize');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Prevents MongDB injection
+// By default, $ and . characters are removed completely from user-supplied input
+// in the following places: req.body, req.params, req.headers, req.query
+// For more:
+// - https://github.com/fiznool/express-mongo-sanitize#usage
+// - https://javascript.plainenglish.io/how-to-sanitize-your-express-app-against-mongodb-injection-cross-site-scripting-6a22f4e822aa
+app.use(mongoSanitize());
+
 app.use('/url', routes);
 
 // app.listen(process.env.PORT || 5000);
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { body, validationResult } = require('express-validator');
 const mongoose = require('mongoose');
 const ShortUrl = require('./models/shortUrl');
 const routes = express.Router();
@@ -9,10 +10,21 @@ mongoose.connect(process.env.MONGODB_URI, {
 });
 
 // Add new url to DB
-routes.post('/shorten', async (req, res) => {
-    const urlDetails = await ShortUrl.create({ full: req.body.fullUrl });
-    res.status(200).send(urlDetails);
-});
+routes.post(
+    '/shorten',
+    body('fullUrl').trim(),
+    body('fullUrl', 'Invalid URL').isURL({ protocols: ['http','https'], require_protocol: true }),
+    async (req, res) => {
+        // Finds the validation errors in this request and wraps them in an object with handy functions
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const urlDetails = await ShortUrl.create({ full: req.body.fullUrl });
+        res.status(200).send(urlDetails);
+    }
+);
 
 // Redirect short to long url
 routes.get('/:shortUrl', async (req, res) => {
